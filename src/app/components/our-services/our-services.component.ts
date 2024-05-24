2f353c5f44495c9872163718037c397baa06bb92
kishanivotion <kishan@ivotiontech.com>
2024-05-24 21:36:28 +0530

    changes done

--- src/app/components/our-services/our-services.component.ts
+++ src/app/components/our-services/our-services.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-our-services',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./our-services.component.scss']
 })
 export class OurServicesComponent {
+
+  constructor(
+    private router: Router,
+  ) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  routelink() {
+    this.router.navigateByUrl('/register')
+  }
 
 }
